c5fb1cd4315abfb78406ed53671535a00d062509
Sooraj Sajil Jiji Christilda(UST <299812@ust.com>
2026-01-14 11:17:01 +0530

    updated patient.ts

--- pharmacy-app/src/api/patient.ts
+++ pharmacy-app/src/api/patient.ts
@@ -1,26 +1,3 @@
-// import api from "./axiosInstance";
-// import { ENDPOINTS } from "./endpoints";
-// import type { CreatePatientRequest, UpdatePatientRequest, PatientDetailsDto, PatientSummaryDto } from "../store/patient/patienttype";
-
-// export const createPatient = async (request: CreatePatientRequest) => {
-//   const response = await api.post(ENDPOINTS.patients, request);
-//   return response.data as { patientId: string };
-// };
-
-// export const updatePatient = async (id: string, request: UpdatePatientRequest) => {
-//   const response = await api.put(`${ENDPOINTS.patientDetails}/${id}`, request);
-//   return response.data;
-// };
-
-// export const getPatientDetails = async (id: string) => {
-//   const response = await api.get(`${ENDPOINTS.patientDetails}/${id}`);
-//   return response.data as PatientDetailsDto;
-// };
-
-// export const searchPatients = async (query: string) => {
-//   const response = await api.get(ENDPOINTS.patientSearch, { params: { query } });
-//   return response.data as PatientSummaryDto[];
-// };
 import api from "./axiosInstance";
 import { ENDPOINTS } from "./endpoints";
 import type {
